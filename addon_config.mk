caed8f4ab10de32c50931b660d13980efc49c36a
Memo Akten <memo@memo.tv>
2019-02-01 02:53:53 +0300

    addon_config and example qtcreator project

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSAFluid
+	ADDON_DESCRIPTION = C++ openFrameworks addon for Stam style Fluid Solver
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = "Fluid"
+	ADDON_URL = https://github.com/memo/ofxMSAFluid
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxMSACore
